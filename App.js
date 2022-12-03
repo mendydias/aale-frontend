d16e3fc0cfc368a099b2b9b763d93bf21fbd76a2
AnjanaKaween <115578094+AnjanaKaween@users.noreply.github.com>
2022-12-03 20:41:05 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-
-
 import VerifcationWindow from './src/screen/VerificationScreen';
 import LogRegisterWindow from './src/screen/LogRegisterScreen';
 
